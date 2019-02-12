7bfda770c0637a21e08dd4269c12346eb32d06c5
mguida22 <michael.guida@colorado.edu>
2019-02-12 10:11:26 -0700

    show start/end locations in time

--- map.js
+++ map.js
@@ -1,7 +1,14 @@
+const SECOND = 1000
+const MINUTE = SECOND * 60
+const DAY = MINUTE * 60 * 24
+const TIME_INTERVAL = MINUTE * 4
 let stations = {}
+let trips = []
 let tripMarkers = {}
+let counter = 0
+let interval = 0
 
-const mymap = L.map('mapid').setView([41.87, -87.63], 10)
+const mymap = L.map('mapid').setView([41.87, -87.63], 11)
 
 L.tileLayer(
   'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
@@ -21,44 +28,101 @@ const saveStations = incomingStations => {
   })
 }
 
-const displayTrips = trips => {
-  trips.forEach(trip => {
-    tripMarkers[trip.trip_id] = {}
-
-    // abort if we don't know about the station
-    if (!stations[trip.from_station_id] || !stations[trip.to_station_id]) {
-      return
-    }
-
-    let lat = stations[trip.from_station_id].latitude
-    let long = stations[trip.from_station_id].longitude
-    tripMarkers[trip.trip_id].start = L.circle([lat, long], {
-      color: 'green',
-      opacity: 0.5,
-      fillColor: 'green',
-      fillOpacity: 0.1,
-      radius: 50
-    }).addTo(mymap)
-
-    lat = stations[trip.to_station_id].latitude
-    long = stations[trip.to_station_id].longitude
-    tripMarkers[trip.trip_id].end = L.circle([lat, long], {
-      color: 'red',
-      opacity: 0.5,
-      fillColor: 'red',
-      fillOpacity: 0.1,
-      radius: 50
-    }).addTo(mymap)
-  })
+const saveTrips = incomingTrips => {
+  trips = incomingTrips
 }
 
-bikeid: '848'
-end_time: '2018-12-07 00:55:53'
-from_station_id: '60'
-start_time: '2018-12-07 00:00:34'
-to_station_id: '484'
-trip_id: '21621607'
-tripduration: '3,319.0'
+const filterTrips = (time, station_id) => {
+  // abort if we don't know about the station
+  if (!stations[station_id]) {
+    return
+  }
+
+  // time of the day in ms
+  timeOfDay = Date.parse(time) - Date.parse('2018-12-07 00:00:00')
+  return timeOfDay > counter && timeOfDay < counter + TIME_INTERVAL
+}
+
+const displayTrips = () => {
+  const startTrips = trips.filter(trip =>
+    filterTrips(trip.start_time, trip.from_station_id)
+  )
+  const endTrips = trips.filter(trip =>
+    filterTrips(trip.end_time, trip.to_station_id)
+  )
+  counter += TIME_INTERVAL
+  if (counter >= DAY) {
+    window.clearInterval(interval)
+  }
+
+  console.log(startTrips.length, endTrips.length, counter)
+
+  startTrips.forEach(trip => {
+    placeTripMarker(trip, true)
+  })
+
+  endTrips.forEach(trip => {
+    placeTripMarker(trip, false)
+  })
+
+  window.setTimeout(() => {
+    startTrips.forEach(trip => {
+      removeTripMarker(trip.trip_id, true)
+    })
+    endTrips.forEach(trip => {
+      removeTripMarker(trip.trip_id, false)
+    })
+  }, 90)
+}
+
+const removeTripMarker = (trip_id, isStart) => {
+  console.log('yo', trip_id)
+  // pass if we don't have a marker
+  if (!tripMarkers[trip_id]) {
+    return
+  }
+
+  if (isStart) {
+    mymap.removeLayer(tripMarkers[trip_id].start)
+  } else {
+    mymap.removeLayer(tripMarkers[trip_id].end)
+  }
+}
+
+const placeTripMarker = (trip, isStart) => {
+  // abort if we don't know about the station
+  if (!stations[trip.from_station_id] || !stations[trip.to_station_id]) {
+    return
+  }
+
+  // if there's no start marker, don't show the end
+  if (!isStart && !tripMarkers[trip.trip_id]) {
+    return
+  }
+
+  let lat = stations[trip.to_station_id].latitude
+  let long = stations[trip.to_station_id].longitude
+  let newMarker = L.circle([lat, long], {
+    color: isStart ? 'green' : 'red',
+    opacity: 0.5,
+    fillColor: isStart ? 'green' : 'red',
+    fillOpacity: 0.1,
+    radius: 50
+  }).addTo(mymap)
+
+  if (isStart) {
+    tripMarkers[trip.trip_id] = {
+      start: newMarker
+    }
+  } else {
+    tripMarkers[trip.trip_id].end = newMarker
+  }
+}
+
+const startAnimation = () => {
+  counter = 0
+  interval = window.setInterval(displayTrips, 50)
+}
 
 fetch('./data/stations.json')
   .then(data => data.json())
@@ -66,4 +130,7 @@ fetch('./data/stations.json')
 
 fetch('./data/trips-2018-12-07.json')
   .then(data => data.json())
-  .then(res => displayTrips(res))
+  .then(res => {
+    saveTrips(res)
+    startAnimation()
+  })
